6ff9af3718c1882eecac2c1b708bbca9d335e793
kd365 <kathleen.hill322@outlook.com>
2026-03-21 23:46:21 -0400

    Add CI/CD pipeline with GitHub Actions, OIDC auth, and remote Terraform state

---
+++ backend.tf
@@ -0,0 +1,10 @@
+# backend.tf
+
+terraform {
+  backend "s3" {
+    bucket         = "tf-readme-generator-state-riq9cbqr"
+    key            = "global/s3/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "readme-generator-tf-locks-KH"
+  }
+}
